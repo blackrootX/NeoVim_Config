13f297e0acf11fd6681523153b39e86fb5277a77
Songsheng Yang <rootpigx@outlook.com>
2025-03-25 02:28:20 +0800

    added some more plugins

--- init.lua
+++ init.lua
@@ -4,11 +4,11 @@ require("config.lazy")
 
  
 if vim.g.neovide then
-  vim.o.guifont = "Maple Mono:h15"
+  vim.o.guifont = "MonoLisa:h15"
   vim.g.neovide_refresh_rate = 120
   vim.g.neovide_refresh_rate_idle = 30
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
-  vim.g.neovide_cursor_vfx_particle_lifetime = 0.5
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1
   vim.g.neovide_remember_window_size = true
   
 end
